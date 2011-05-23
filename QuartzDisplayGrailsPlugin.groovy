648f8cad81f01ca2f9e8d2ad418f3317ddcc1dff
James Cook <james.cookie@gmail.com>
2011-05-23 21:40:06 +0100

    Depends on quartz

--- QuartzDisplayGrailsPlugin.groovy
+++ QuartzDisplayGrailsPlugin.groovy
@@ -4,7 +4,7 @@ class QuartzDisplayGrailsPlugin {
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.3.7 > *"
     // the other plugins this plugin depends on
-    def dependsOn = [:]
+    def dependsOn = [quartz:"0.4.2"]
     // resources that are excluded from plugin packaging
     def pluginExcludes = [
             "grails-app/views/error.gsp"
