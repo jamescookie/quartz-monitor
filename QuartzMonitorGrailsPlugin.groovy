08679b70b108e519b2e7d6e0011c2f980cfb1f75
James Cook <james.cookie@gmail.com>
2011-06-30 21:52:33 +0100

    Update details

--- QuartzMonitorGrailsPlugin.groovy
+++ QuartzMonitorGrailsPlugin.groovy
@@ -1,54 +1,25 @@
 import org.grails.plugins.quartz.QuartzMonitorJobFactory
 
 class QuartzMonitorGrailsPlugin {
-    // the plugin version
     def version = "0.1"
-    // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.3.7 > *"
-    // the other plugins this plugin depends on
     def dependsOn = [quartz:"0.4.2"]
-    // resources that are excluded from plugin packaging
     def pluginExcludes = [
             "grails-app/views/error.gsp"
     ]
 
-    // TODO Fill in these fields
-    def author = "Your name"
-    def authorEmail = ""
-    def title = "Plugin summary/headline"
+    def author = "James Cook"
+    def authorEmail = "grails@jamescookie.com"
+    def title = "Monitor your Quartz jobs"
     def description = '''\\
-Brief description of the plugin.
+One clear and concise page that enables you to administer all your Quartz jobs.
 '''
 
-    // URL to the plugin's documentation
     def documentation = "http://grails.org/plugin/quartz-monitor"
-
-    def doWithWebDescriptor = { xml ->
-        // TODO Implement additions to web.xml (optional), this event occurs before 
-    }
 
     def doWithSpring = {
         quartzJobFactory(QuartzMonitorJobFactory) {
             sessionFactory = ref("sessionFactory")
         }
-    }
-
-    def doWithDynamicMethods = { ctx ->
-        // TODO Implement registering dynamic methods to classes (optional)
-    }
-
-    def doWithApplicationContext = { applicationContext ->
-        // TODO Implement post initialization spring config (optional)
-    }
-
-    def onChange = { event ->
-        // TODO Implement code that is executed when any artefact that this plugin is
-        // watching is modified and reloaded. The event contains: event.source,
-        // event.application, event.manager, event.ctx, and event.plugin.
-    }
-
-    def onConfigChange = { event ->
-        // TODO Implement code that is executed when the project configuration changes.
-        // The event is the same as for 'onChange'.
     }
 }
