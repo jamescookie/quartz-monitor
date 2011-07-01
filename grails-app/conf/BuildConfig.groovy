7a9a2ae35fae243eaa7833deb07d90436c9709ae
James Cook <james.cookie@gmail.com>
2011-07-01 08:28:35 +0100

    Rely on quartz at compile time

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -23,6 +23,9 @@ grails.project.dependency.resolution = {
         //mavenRepo "http://download.java.net/maven/2/"
         //mavenRepo "http://repository.jboss.com/maven2/"
     }
+    plugins {
+        compile ':quartz:0.4.2'
+    }
     dependencies {
         // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
 
