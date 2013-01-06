7a0221ee9d558789f58eb16faf638dcf19aa279e
James Cook <james.cookie@gmail.com>
2013-01-06 22:28:10 +0000

    Reflect the grails compatibility of quartz plugin

--- QuartzMonitorGrailsPlugin.groovy
+++ QuartzMonitorGrailsPlugin.groovy
@@ -2,7 +2,7 @@ import org.grails.plugins.quartz.QuartzMonitorJobFactory
 
 class QuartzMonitorGrailsPlugin {
     def version = "0.3-RC1"
-    def grailsVersion = "2.0 > *"
+    def grailsVersion = "1.2 > *"
     def dependsOn = [quartz:"1.0-RC4"]
     def pluginExcludes = [
             "grails-app/views/error.gsp"
