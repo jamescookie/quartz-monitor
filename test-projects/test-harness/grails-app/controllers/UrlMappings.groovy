b104f80a45eef36694e3cc23a16d6b191e72c94f
James Cook <james.cookie@gmail.com>
2015-05-01 21:49:59 +0100

    Updated test project to grails 3.0.1

--- test-projects/test-harness/grails-app/controllers/UrlMappings.groovy
+++ test-projects/test-harness/grails-app/controllers/UrlMappings.groovy
@@ -1,6 +1,6 @@
 class UrlMappings {
 
-	static mappings = {
+    static mappings = {
         "/$controller/$action?/$id?(.$format)?"{
             constraints {
                 // apply constraints here
@@ -9,5 +9,6 @@ class UrlMappings {
 
         "/"(view:"/index")
         "500"(view:'/error')
-	}
+        "404"(view:'/notFound')
+    }
 }
