6ea66cac4c507033c0c222a04d6bca322722d247
James Cook <james.cookie@gmail.com>
2015-05-01 21:49:59 +0100

    Updated test project to grails 3.0.1

--- test-projects/test-harness/grails-app/assets/javascripts/application.js
+++ test-projects/test-harness/grails-app/assets/javascripts/application.js
@@ -5,16 +5,16 @@
 // You're free to add application-wide JavaScript to this file, but it's generally better 
 // to create separate JavaScript files as needed.
 //
-//= require jquery
+//= require jquery-2.1.3.js
 //= require_tree .
 //= require_self
 
 if (typeof jQuery !== 'undefined') {
-	(function($) {
-		$('#spinner').ajaxStart(function() {
-			$(this).fadeIn();
-		}).ajaxStop(function() {
-			$(this).fadeOut();
-		});
-	})(jQuery);
+    (function($) {
+        $('#spinner').ajaxStart(function() {
+            $(this).fadeIn();
+        }).ajaxStop(function() {
+            $(this).fadeOut();
+        });
+    })(jQuery);
 }
