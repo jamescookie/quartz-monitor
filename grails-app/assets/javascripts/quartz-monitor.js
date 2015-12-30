347ca6ff145a50d157f9830885fdaa78c3f9531a
Markus Paaso <markus.paaso@a-lehdet.fi>
2015-12-30 13:03:21 +0200

    Fix for https://github.com/jamescookie/quartz-monitor/issues/32

--- grails-app/assets/javascripts/quartz-monitor.js
+++ grails-app/assets/javascripts/quartz-monitor.js
@@ -1,4 +1,4 @@
-//=require ${grails.util.Holders.config.quartz.monitor.showCountdown ? 'jquery.countdown' : 'quartz-monitor-main'}
-//=require ${grails.util.Holders.config.quartz.monitor.showCountdown ? 'jquery.color' : 'quartz-monitor-main'}
-//=require ${grails.util.Holders.config.quartz.monitor.showTickingClock ? 'jquery.clock' : 'quartz-monitor-main'}
+//=require ${grails.util.Holders.config?.quartz?.monitor?.showCountdown ? 'jquery.countdown' : 'quartz-monitor-main'}
+//=require ${grails.util.Holders.config?.quartz?.monitor?.showCountdown ? 'jquery.color' : 'quartz-monitor-main'}
+//=require ${grails.util.Holders.config?.quartz?.monitor?.showTickingClock ? 'jquery.clock' : 'quartz-monitor-main'}
 //=require quartz-monitor-main
