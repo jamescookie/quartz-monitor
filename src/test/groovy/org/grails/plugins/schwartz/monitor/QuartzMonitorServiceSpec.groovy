fa883ec8155b6690f1f4556e398f98578d100da3
Robert Oschwald <roos@symentis.com>
2016-11-07 21:45:38 +0100

    unit test

--- src/test/groovy/org/grails/plugins/schwartz/monitor/QuartzMonitorServiceSpec.groovy
+++ src/test/groovy/org/grails/plugins/schwartz/monitor/QuartzMonitorServiceSpec.groovy
@@ -1,6 +1,7 @@
 package org.grails.plugins.schwartz.monitor
 
 import grails.test.mixin.TestFor
+import org.quartz.JobKey
 import spock.lang.Specification
 
 /**
@@ -15,8 +16,59 @@ class QuartzMonitorServiceSpec extends Specification {
     def cleanup() {
     }
 
-    void "test something"() {
-        expect:"fix me"
-            true == false
+    void "test updateJobStartDate"() {
+        given:
+        def date = new Date()
+        def jobKey = JobKey.jobKey("test1")
+
+
+        when: "Date is stored"
+        service.updateJobStartDate(jobKey, date)
+
+        then:
+        service.getAdditionalJobDetails(jobKey) != null
+        service.getAdditionalJobDetails(jobKey)."${QuartzMonitorService.START_DATE}" == date
+    }
+
+    void "test updateJobEndDate"() {
+        given:
+        def date = new Date()
+        def jobKey = JobKey.jobKey("test2")
+
+
+        when: "EndDate is stored"
+        service.updateJobEndDate(jobKey, date)
+
+        then:
+        service.getAdditionalJobDetails(jobKey) != null
+        service.getAdditionalJobDetails(jobKey)."${QuartzMonitorService.END_DATE}" == date
+    }
+
+    void "test updateJobStartDate NULL"() {
+        given:
+        def date = null
+        def jobKey = JobKey.jobKey("test3")
+
+
+        when: "StartDate null is stored"
+        service.updateJobStartDate(jobKey, date)
+
+        then:
+        service.getAdditionalJobDetails(jobKey) != null
+        service.getAdditionalJobDetails(jobKey)."${QuartzMonitorService.START_DATE}" == date
+    }
+
+    void "test updateJobEndDate NULL"() {
+        given:
+        def date = null
+        def jobKey = JobKey.jobKey("test4")
+
+
+        when: "EndDate null is stored"
+        service.updateJobEndDate(jobKey, date)
+
+        then:
+        service.getAdditionalJobDetails(jobKey) != null
+        service.getAdditionalJobDetails(jobKey)."${QuartzMonitorService.END_DATE}" == date
     }
 }
