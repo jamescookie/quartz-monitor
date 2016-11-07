9304b20cabc7246bc33841a57a8789e6e469e01f
Robert Oschwald <roos@symentis.com>
2016-11-07 20:45:12 +0100

    - add test

---
+++ src/test/groovy/org/grails/plugins/schwartz/monitor/QuartzMonitorServiceSpec.groovy
@@ -0,0 +1,22 @@
+package org.grails.plugins.schwartz.monitor
+
+import grails.test.mixin.TestFor
+import spock.lang.Specification
+
+/**
+ * See the API for {@link grails.test.mixin.services.ServiceUnitTestMixin} for usage instructions
+ */
+@TestFor(QuartzMonitorService)
+class QuartzMonitorServiceSpec extends Specification {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+        expect:"fix me"
+            true == false
+    }
+}
