4228b1016495524587836a4c5aa4c71276826742
Robert Oschwald <robertoschwald@googlemail.com>
2018-12-02 14:57:10 +0100

    avoid errors when jQuery not available

--- grails-app/assets/javascripts/schwartz-monitor-main.js
+++ grails-app/assets/javascripts/schwartz-monitor-main.js
@@ -1,86 +1,87 @@
-if (jQuery) {
-	(function($, window) {
-		var
-        xOffset = 10,
-		yOffset = 20,
-        init = function() {
-            $('.quartz-to-hide').hide();
-			$('.quartz-tooltip')
-                .hover(function(e){
-                    var tooltipData = $(this).data('tooltip');
-                    if (tooltipData === "") return;
-                    displayToolTip(tooltipData, (e.pageX + yOffset), (e.pageY - xOffset));
-                },
-                function(){
-                    $("#quartz-tooltip").remove();
-                })
-                .mousemove(function(e){
-                    $("#quartz-tooltip")
-                        .css("top",(e.pageY - xOffset) + "px")
-                        .css("left",(e.pageX + yOffset) + "px");
-                });
-            $('.quartz-countdown').each(function() {
-                var item = $(this),
-                    remaining = item.data('next-run');
-                if (remaining === "") return;
-                countdown(item, remaining);
-            });
-           displayClock($('#clock'));
-		},
+if(typeof jQuery !== 'undefined'){
+  (function($, window){
+    var
+      xOffset = 10,
+      yOffset = 20,
+      init = function(){
+        $('.quartz-to-hide').hide();
+        $('.quartz-tooltip')
+          .hover(function(e){
+              var tooltipData = $(this).data('tooltip');
+              if(tooltipData === "") return;
+              displayToolTip(tooltipData, (e.pageX + yOffset), (e.pageY - xOffset));
+            },
+            function(){
+              $("#quartz-tooltip").remove();
+            })
+          .mousemove(function(e){
+            $("#quartz-tooltip")
+              .css("top", (e.pageY - xOffset) + "px")
+              .css("left", (e.pageX + yOffset) + "px");
+          });
+        $('.quartz-countdown').each(function(){
+          var item = $(this),
+            remaining = item.data('next-run');
+          if(remaining === "") return;
+          countdown(item, remaining);
+        });
+        displayClock($('#clock'));
+      },
 
-        displayClock = function(item) {
-            if (item.clock !== undefined) {
-                item.clock({"timestamp":new Date(item.data('time'))});
-            }
-        },
+      displayClock = function(item){
+        if(item.clock !== undefined){
+          item.clock({"timestamp":new Date(item.data('time'))});
+        }
+      },
 
-        reloadPage = function() {
-            var original = $(this).css('color');
-            var delay = 200;
+      reloadPage = function(){
+        var original = $(this).css('color');
+        var delay = 200;
+        $(this).animate({
+          backgroundColor:'#f00',
+          color:'#fff'
+        }, delay, function(){
+          $(this).animate({
+            backgroundColor:'transparent',
+            color:original
+          }, delay, function(){
             $(this).animate({
-                backgroundColor: '#f00',
-                color: '#fff'
-            }, delay, function() {
-                $(this).animate({
-                    backgroundColor: 'transparent',
-                    color: original
-                }, delay, function() {
-                    $(this).animate({
-                        backgroundColor: '#f00',
-                        color: '#fff'
-                    }, delay, function() {
-                        $(this).animate({
-                            backgroundColor: 'transparent',
-                            color: original
-                        }, delay, function() {
-                             location.reload(true);
-                        });
-                    });
-                });
+              backgroundColor:'#f00',
+              color:'#fff'
+            }, delay, function(){
+              $(this).animate({
+                backgroundColor:'transparent',
+                color:original
+              }, delay, function(){
+                location.reload(true);
+              });
             });
-        },
+          });
+        });
+      },
 
-        countdown = function(item, remaining) {
-            if (item.countdown !== undefined) {
-                item.countdown(
-                   {until: new Date(remaining),
-                       onExpiry: reloadPage,
-                       compact: true
-                   });
-            }
-        },
+      countdown = function(item, remaining){
+        if(item.countdown !== undefined){
+          item.countdown(
+            {
+              until:new Date(remaining),
+              onExpiry:reloadPage,
+              compact:true
+            });
+        }
+      },
 
-        displayToolTip = function(tooltipData, x, y) {
-            $('<p></p>')
-                    .text(tooltipData)
-                    .attr('id', 'quartz-tooltip')
-                    .css("top", y + "px")
-                    .css("left", x + "px")
-                    .appendTo('body')
-                    .fadeIn("fast");
-        };
+      displayToolTip = function(tooltipData, x, y){
+        $('<p></p>')
+          .text(tooltipData)
+          .attr('id', 'quartz-tooltip')
+          .css("top", y + "px")
+          .css("left", x + "px")
+          .appendTo('body')
+          .fadeIn("fast");
+      };
 
-		$(init);
+    $(init);
 
-	}(jQuery, this));
+  }(jQuery, this));
 }
