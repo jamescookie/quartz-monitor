1630920f50720fdf827a6f476bbc139de678d163
Robert Oschwald <robertoschwald@googlemail.com>
2019-03-01 10:33:03 +0100

    reworked gradle project structure

--- doc.sh
+++ doc.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd "$DIR"
+
+cd plugin
+
 rm -rf docs/manual
 grails doc --pdf --stacktrace | grep -v javadoc
 rm -rf docs/manual/api
 rm -rf docs/manual/gapi
 grails add-tracking
+cd ..
--- travis-build.sh
+++ travis-build.sh
@@ -9,18 +9,50 @@ echo "TRAVIS_PULL_REQUEST: $TRAVIS_PULL_REQUEST"
 echo "TRAVIS_TAG         : $TRAVIS_TAG"
 
 rm -rf build
-./gradlew clean check install --stacktrace || EXIT_STATUS=$?
 
-if [[ $EXIT_STATUS -eq 0 ]]; then
-	./integration-test-app/run_integration_tests.sh || EXIT_STATUS=$?
+echo "*******************************"
+echo "schwartz-monitor:check"
+echo "*******************************"
+./gradlew :schwartz-monitor:check --no-daemon --console=plain --stacktrace || EXIT_STATUS=$?
+
+if [ $EXIT_STATUS -ne 0 ]; then
+  echo "schwartz-monitor:check failed => exit $EXIT_STATUS"
+  exit $EXIT_STATUS
 fi
 
-if [[ -n $TRAVIS_TAG && $TRAVIS_PULL_REQUEST == 'false' && $EXIT_STATUS -eq 0 ]]; then
-  echo "Uploading to bintray"
-	./gradlew bintrayUpload --stacktrace
+echo "*******************************"
+echo "schwartz-monitor:install"
+echo "*******************************"
+./gradlew :schwartz-monitor:install --no-daemon --console=plain --stacktrace || EXIT_STATUS=$?
+
+if [ $EXIT_STATUS -ne 0 ]; then
+  echo "schwartz-monitor:install failed => exit $EXIT_STATUS"
+  exit $EXIT_STATUS
+fi
+
+echo "*******************************"
+echo "integration-test-app:check"
+echo "*******************************"
+
+./gradlew :integration-test-app:check --no-daemon --console=plain --stacktrace || EXIT_STATUS=$?
+
+if [ $EXIT_STATUS -ne 0 ]; then
+  echo "integration-test-app:check failed => exit $EXIT_STATUS"
+  exit $EXIT_STATUS
+fi
+
+
+# Only publish if the branch is on master, and not a PR
+if [[ -n $TRAVIS_TAG ]] || [[ $TRAVIS_BRANCH == 'master' && $TRAVIS_PULL_REQUEST == 'false' ]]; then
+
+  if [[ -n $TRAVIS_TAG ]]; then
+      echo "Pushing build to Bintray"
+      ./gradlew :schwartz-monitor:bintrayUpload || EXIT_STATUS=$?
+  fi
 
   echo "Building Docs"
-	./gradlew docs --stacktrace
+	./gradlew schwartz-monitor:docs --stacktrace || EXIT_STATUS=$?
+
 	git config --global user.name "$GIT_NAME"
 	git config --global user.email "$GIT_EMAIL"
 	git config --global credential.helper "store --file=~/.git-credentials"
@@ -30,18 +62,43 @@ if [[ -n $TRAVIS_TAG && $TRAVIS_PULL_REQUEST == 'false' && $EXIT_STATUS -eq 0 ]]
 	git clone https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git -b gh-pages gh-pages --single-branch > /dev/null
 	cd gh-pages
 
-	rm -rf latest
-	mkdir latest
-	cp -r ../build/docs/. ./latest/
+  # If this is the master branch then update the snapshot
+  if [[ $TRAVIS_BRANCH == 'master' ]]; then
 
-	mv -f latest/ghpages.html index.html
-	git add .
+    mv ../plugin/build/docs/ghpages.html index.html
+    git add index.html
+
+    mkdir -p snapshot
+    cp -r ../plugin/build/docs/. ./snapshot/
+    git add snapshot/*
+  fi
+
+  # TAG exists > is latest docs
+  if [[ -n ${TRAVIS_TAG} ]]; then
+        git rm -rf latest/
+        mkdir -p latest
+        cp -r ../plugin/build/docs/. ./latest/
+        git add latest/*
+
+        version="$TRAVIS_TAG" # eg: v3.0.1
+        version=${version:1} # 3.0.1
+        majorVersion=${version:0:4} # 3.0.
+        majorVersion="${majorVersion}x" # 3.0.x
+
+        mkdir -p "$version"
+        cp -r ../plugin/build/docs/. "./$version/"
+        git add "$version/*"
+
+        git rm -rf "$majorVersion"
+        cp -r ../plugin/build/docs/. "./$majorVersion/"
+        git add "$majorVersion/*"
+  fi
 
 	git commit -a -m "Updating docs for Travis build: https://travis-ci.org/$TRAVIS_REPO_SLUG/builds/$TRAVIS_BUILD_ID"
-
 	echo "Pusing gh-pages to $TRAVIS_REPO_SLUG"
 	git push origin gh-pages
-
+  cd ..
+  rm -rf gh-pages
 fi
 
 exit $EXIT_STATUS
